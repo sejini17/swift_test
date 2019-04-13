356d5d32d8b5966f1a505b1f782dc82528b07d45
그린아카데미 <greengn@geulin-akademiui-imac.local>
2019-04-13 12:44:46 +0900

    test

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -9,7 +9,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var lblInput: UILabel!
+    @IBOutlet var txtInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +22,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func btnInput(_ sender: Any) {
+        lblInput.text = "efjkafjelkj" + txtInput.text!
+    }
+    
 }
 
