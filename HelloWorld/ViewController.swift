ad5554e0d76b1886df04480e34c634e9821a8be1
그린아카데미 <greengn@geulin-akademiui-imac.local>
2019-04-13 11:03:22 +0900

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by 그린아카데미 on 2019. 4. 13..
+//  Copyright © 2019년 sjk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
